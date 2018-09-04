9dd75d6c1df9016056b14a90ff7386574297c86f
guan <kuniyasu.guan@gmail.com>
2018-09-04 19:50:49 +0900

    slug test

--- pages/__test__/_slug.test.js
+++ pages/__test__/_slug.test.js
@@ -6,14 +6,27 @@ import merge from 'lodash.merge'
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
+let todos
+
 function createStore(overrides) {
-  let todos = [
+  todos = [
     { title: 'one', completed: false },
     { title: 'two', completed: true }
   ]
+
+  const activeTodos = [
+    { title: 'one', completed: false }
+  ]
+
+  const completedTodos = [
+    { title: 'two', completed: true }    
+  ]
+
   const defaultStoreConfig = {
     getters: {
-      allTodos: () => todos
+      allTodos: () => todos,
+      activeTodos: () => activeTodos,
+      completedTodos: () => completedTodos
     }
   }
   return new Vuex.Store(
@@ -36,84 +49,123 @@ function createWrapper(overrides) {
 
 describe('_slug.vue', () => {
 
-  it('render slug', () => {
-    const store = createStore({})
-    const wrapper = createWrapper({ store })
-    const items = wrapper.findAll('li')
-    expect(items).toHaveLength(2)
-  })
-
-  it('Mark all as complete', () => {
-    const store = createStore({})
-    store.dispatch = jest.fn()
-    const wrapper = createWrapper({ store })
-    wrapper.find('.toggle').trigger('click')
-    expect(store.dispatch).toHaveBeenCalledWith('allDone')
-  })
-
-  it('1個目のタスクをdoneにする', () => {
-    const store = createStore()
-    const wrapper = createWrapper({ store })
-    expect(wrapper.vm.todos[0].completed).toBeFalsy()    
-    let checkbox = wrapper.findAll('li').at(0).find('input[type="checkbox"]')    
-    checkbox.setChecked()
-    expect(wrapper.vm.todos[0].completed).toBeTruthy()
-  })
-
-  it('2個目のタスクを削除する', () => {
-    const store = createStore()
-    store.dispatch = jest.fn()
-    const wrapper = createWrapper({ store })
-    expect(wrapper.vm.todos).toHaveLength(2)
-
-    let task = wrapper.findAll('li').at(1)
-    task.find('.destroy').trigger('click')
-    const expectedData = expect.objectContaining({
-      completed: true,
-      title: 'two'
+  describe('active', () => {
+    it('todoリストはactiveなのが1つある', () => {
+      const store = createStore()
+      const mocks = {
+        $route: {
+          params: { slug: 'active' }
+        }      
+      }
+      const wrapper = createWrapper({ mocks, store })
+      expect(wrapper.vm.todos).toHaveLength(1)
+      expect(wrapper.vm.todos[0].completed).toBeFalsy()
     })
-    expect(store.dispatch).toHaveBeenCalledWith('removeTodo', expectedData)
   })
 
-  it('1個目のタスクを編集する', () => {
-    const store = createStore()
-    const wrapper = createWrapper({ store })
+  describe('completed', () => {
+    it('todoリストはcompletedなのが1つある', () => {
+      const store = createStore()
+      const mocks = {
+        $route: {
+          params: { slug: 'completed' }
+        }
+      }
 
-    let task = wrapper.findAll('li').at(0)
-    task.find('label').trigger('dbclick')
-    let editInput = task.find('.edit')
-    editInput.setValue('Hoge')
-    editInput.trigger('keyup.enter')
-    expect(wrapper.vm.todos[0].title).toBe('Hoge')
-  })
-
-  it('タスクを編集しても、タイトルが入力されていないと削除される', () => {
-    const store = createStore()
-    store.dispatch = jest.fn()
-    const wrapper = createWrapper({ store })
-    
-    let task = wrapper.findAll('li').at(0)
-    task.find('label').trigger('dbclick')
-    let editInput = task.find('.edit')
-    editInput.setValue('')
-    editInput.trigger('keyup.enter')
-    const expectedData = expect.objectContaining({
-      title: "",
-      completed: false
+      const wrapper = createWrapper({ mocks, store })
+      expect(wrapper.vm.todos).toHaveLength(1)
+      expect(wrapper.vm.todos[0].completed).toBeTruthy()
     })
-    expect(store.dispatch).toHaveBeenCalledWith('removeTodo', expectedData)
   })
 
-  it('編集をキャンセル', () => {
-    const store = createStore()
-    let wrapper = createWrapper({ store })
-    wrapper.vm.beforeEditCache = 'one'
-
-    let task = wrapper.findAll('li').at(0)
-    task.find('label').trigger('dbclick')
-    let editInput = task.find('.edit')
-    editInput.setValue('Hoge')
-    editInput.trigger('keyup.esc')
-    expect(wrapper.vm.todos[0].title).toBe('one')
+  describe('all', () => {
+    it('render slug', () => {
+      const store = createStore({})
+      const wrapper = createWrapper({ store })
+      const items = wrapper.findAll('li')
+      expect(items).toHaveLength(2)
+    })
+  
+    it('Mark all as complete', () => {
+      const store = createStore({})
+      store.dispatch = jest.fn()
+      const wrapper = createWrapper({ store })
+      wrapper.find('.toggle').trigger('click')
+      expect(store.dispatch).toHaveBeenCalledWith('allDone')
+    })
+  
+    it('1個目のタスクをdoneにする', () => {
+      const store = createStore()
+      const wrapper = createWrapper({ store })
+      expect(wrapper.vm.todos[0].completed).toBeFalsy()    
+      let checkbox = wrapper.findAll('li').at(0).find('input[type="checkbox"]')    
+      checkbox.setChecked()
+      expect(wrapper.vm.todos[0].completed).toBeTruthy()
+    })
+  
+    it('2個目のタスクを削除する', () => {
+      const store = createStore()
+      store.dispatch = jest.fn()
+      const wrapper = createWrapper({ store })
+      expect(wrapper.vm.todos).toHaveLength(2)
+  
+      let task = wrapper.findAll('li').at(1)
+      task.find('.destroy').trigger('click')
+      const expectedData = expect.objectContaining({
+        completed: true,
+        title: 'two'
+      })
+      expect(store.dispatch).toHaveBeenCalledWith('removeTodo', expectedData)
+    })
+  
+    it('1個目のタスクを編集する', () => {
+      const store = createStore()
+      const wrapper = createWrapper({ store })
+  
+      let task = wrapper.findAll('li').at(0)
+      task.find('label').trigger('dbclick')
+      let editInput = task.find('.edit')
+      editInput.setValue('Hoge')
+      editInput.trigger('keyup.enter')
+      expect(wrapper.vm.todos[0].title).toBe('Hoge')
+    })
+  
+    it('タスクを編集しても、タイトルが入力されていないと削除される', () => {
+      const store = createStore()
+      store.dispatch = jest.fn()
+      const wrapper = createWrapper({ store })
+      
+      let task = wrapper.findAll('li').at(0)
+      task.find('label').trigger('dbclick')
+      let editInput = task.find('.edit')
+      editInput.setValue('')
+      editInput.trigger('keyup.enter')
+      const expectedData = expect.objectContaining({
+        title: "",
+        completed: false
+      })
+      expect(store.dispatch).toHaveBeenCalledWith('removeTodo', expectedData)
+    })
+  
+    it('編集をキャンセル', () => {
+      const store = createStore()
+      let wrapper = createWrapper({ store })
+      wrapper.vm.beforeEditCache = 'one'
+  
+      let task = wrapper.findAll('li').at(0)
+      task.find('label').trigger('dbclick')
+      let editInput = task.find('.edit')
+      editInput.setValue('Hoge')
+      editInput.trigger('keyup.esc')
+      expect(wrapper.vm.todos[0].title).toBe('one')
+    })
+  
+    it('save', () => {
+      const store = createStore()
+      store.dispatch = jest.fn()
+      let wrapper = createWrapper({ store })
+      wrapper.vm.save()
+      expect(store.dispatch).toHaveBeenCalledWith('saveTodos')    
+    })
   })
 })
